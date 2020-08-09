6cbbdb4d5acd3007d2879aa538bc3a4074730b40
brookes22 <b.brookes@unisaas.co.uk>
2020-08-09 13:11:14 +0100

    Update embed.ts

--- apps/api/src/app/controllers/embed.ts
+++ apps/api/src/app/controllers/embed.ts
@@ -1,3 +1,8 @@
 import { Request, Response } from 'express';
+import { Sprite } from '../models';
 
-export const EmbedController = async (req: Request, res: Response) => {};
+export const EmbedController = async (req: Request, res: Response) => {
+  const slug = req.params.slug;
+  const sprite = await Sprite.findOne({ slug });
+  return res.render('embed', { sprite });
+};
