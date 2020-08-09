0eaaaddea710949824d1f273674c5597900c6c2f
brookes22 <b.brookes@unisaas.co.uk>
2020-08-09 13:14:42 +0100

    :construction: Temp 404 page

--- apps/api/src/app/controllers/embed.ts
+++ apps/api/src/app/controllers/embed.ts
@@ -4,5 +4,10 @@ import { Sprite } from '../models';
 export const EmbedController = async (req: Request, res: Response) => {
   const slug = req.params.slug;
   const sprite = await Sprite.findOne({ slug });
+
+  if (!sprite) {
+    return res.render('404');
+  }
+
   return res.render('embed', { sprite });
 };
