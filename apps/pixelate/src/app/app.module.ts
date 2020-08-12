99fffda5c84aefd462a857164ee1607e6286e3fe
Ben Brookes <ben@pixelbeard.co>
2020-08-12 09:21:42 +0100

    :construction: PWA assets

--- apps/pixelate/src/app/app.module.ts
+++ apps/pixelate/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SharedModule } from './shared';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -54,6 +55,7 @@ import { AppComponent } from './app.component';
         redirectTo: 'gallery',
       },
     ]),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [ApiService, EditorService, GalleryService],
   bootstrap: [AppComponent],
